ca62b9b7726f1892a21937871509d40ecc53c948
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2015-07-11 18:07:29 -0700

    Add feature extractor, init.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -4,3 +4,15 @@ feature_extractor.py
 File consisting of several feature extraction functions, used to create the representative feature
 vector for a given query.
 """
+
+from data
+
+from spacy.en import English
+nlp = English()
+
+def build_keyword_counts(tokens):
+    """Basic cue, extracts keyword counts in tokens, based on clusters in keywords.py"""
+
+
+def build_feature_vector(sentence):
+    """
