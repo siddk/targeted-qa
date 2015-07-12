9e682da2626eac595b715fc64e534cfb1c46e059
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2015-07-11 17:10:09 -0700

    Add data, keywords

---
+++ data/keywords.py
@@ -0,0 +1,16 @@
+"""
+File of tagged key words, for building syn-sets, large scale representative vectors, etc.
+"""
+
+about_keywords = ["about", "interests", "transfer", "leave", "from"]
+
+education_keywords = ["education", "school", "college", "university", "learn", "berkeley",
+                      "brown", "academic", "academics", "courses", "classes", "schoolwork",
+                      "major", "majoring", "concentrate", "concentrating", "studying", "study"]
+
+work_keywords = ["work", "experience", "research", "researching", "working", "lab", "oscii",
+                 "par", "write", "writelab", "autogrid", "grid", "auto", "61a", "ta", "teach",
+                 "teaching", "skills"]
+
+project_keywords = ["project", "projects", "hobby", "hobbies", "free", "time", "papers", "paper",
+                    "portfolio", "showcase"]
